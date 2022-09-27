de36a6be3ca399309f3279dbbe539fc3f6667f43
Zakaria878 <hzakaria3311@gmail.com>
2022-09-27 17:48:26 +0600

    total cost calculate

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import Cart from "../cart/Cart";
+import Product from "../Product/Product";
+import "./Shop.css";
+
+const Shop = () => {
+  const [products, setProduct] = useState([]);
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setProduct(data));
+  }, []);
+
+  const handleAddToCart = (product) => {
+    console.log(product);
+    const newCart = [...cart, product];
+    setCart(newCart);
+  };
+
+  return (
+    <div className="shop-container">
+      <div className="products-container">
+        {products.map((product) => (
+          <Product
+            key={product.id}
+            product={product}
+            handleAddToCart={handleAddToCart}
+          ></Product>
+        ))}
+      </div>
+      <div className="cart-container">
+        <Cart cart={cart}></Cart>
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
